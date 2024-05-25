3f4e8b96417fac6eb8cdd9f8a9190a83145ba91c
Samnang Nuonsinoeun <samnangnuonsineoun2025@u.northwestern.edu>
2024-05-25 17:04:42 -0500

    add templates

--- uv_mhealth/uv_mhealth/settings.py
+++ uv_mhealth/uv_mhealth/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -54,7 +53,7 @@ ROOT_URLCONF = 'uv_mhealth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'uv_mhealth/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -65,6 +64,7 @@ TEMPLATES = [
             ],
         },
     },
+
 ]
 
 WSGI_APPLICATION = 'uv_mhealth.wsgi.application'
